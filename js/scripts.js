23a9d392781e852e043112d4f4d2bb9a353a909e
Eric Conner and Mike Donovan <mike@>
2018-08-13 17:11:59 -0700

    Add functionality to button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
+var wordScores = {}
+var repeatedWords = []
+var uniqueList = []
 $(document).ready(function(){
   $("#submit-button").click(function(){
-    console.log("button is clicked")
+    var rawText = $("#text-input").val()
+    console.log("RAW: " + rawText)
+    var rawArray = rawText.split(" ")
+    console.log("RAW ARRAY: " + rawArray)
+
+
+
+
+
+    rawArray.forEach(function(word){
+      console.log(word)
+      wordScores[word] = 0
+      rawArray.filter(function(wordToCheck){
+        if (word===wordToCheck) {
+          wordScores[word]++
+          var revisedArray = repeatedWords.splice(repeatedWords.indexOf(word),1)
+        }
+      })
+    })
+    uniqueList = Object.keys(wordScores)
+    console.log(wordScores)
+    console.log(repeatedWords)
+    console.log(uniqueList)
   })
 })
